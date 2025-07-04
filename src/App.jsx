a53f772c7fec5bc6b5545a3fcc530d33431114d4
irpanzy <irfanoputra27@gmail.com>
2025-07-04 11:34:46 +0700

    feat: add nested user routes for guide, basket, futsal, and report pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,11 @@ import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import UsersPage from "./pages/UserPage";
 import { Toaster } from "@/components/ui/sonner";
+import Panduan from "./components/users/Panduan";
+import Basket from "./components/users/Basket";
+import Futsal from "./components/users/Futsal";
+import Laporan from "./components/users/Laporan";
+import UsersHome from "./components/users/HomeUser";
 
 function App() {
   return (
@@ -22,7 +27,13 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/admin/*" element={<AdminDashboard />} />
-        <Route path="/users" element={<UsersPage />} />
+        <Route path="/users" element={<UsersPage />}>
+          <Route index element={<UsersHome />} />
+          <Route path="panduan" element={<Panduan />} />
+          <Route path="sewa-lapangan-basket" element={<Basket />} />
+          <Route path="sewa-lapangan-futsal" element={<Futsal />} />
+          <Route path="laporan" element={<Laporan />} />
+        </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
