803a4c01187446c9ac7bdcc29ffda993957cf2ff
irpanzy <irfanoputra27@gmail.com>
2025-07-04 11:35:31 +0700

    feat: create UserDashboard layout with NavbarUser and nested route outlet

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,13 @@
-import { NavbarUser } from "@/components/NavbarUser";
+import { NavbarUser } from "@/components/users/NavbarUser";
+import { Outlet } from "react-router-dom";
 
 export default function UserDashboard() {
   return (
     <>
       <NavbarUser />
+      <div className="mx-auto w-full max-w-7xl px-4">
+        <Outlet />
+      </div>
     </>
   );
 }
